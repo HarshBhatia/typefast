87dec61665f88bd1135d4bc45adbf95ad055853b
Harsh Bhatia <bhatiaharsh.007@gmail.com>
2020-04-11 12:12:05 +0530

    Added boilerplate

--- src/App.js
+++ src/App.js
@@ -3,8 +3,27 @@ import React from "react";
 import words from "./words";
 
 function App() {
-  return <div className="App"></div>;
+  const [testComplete, setTestComplete] = useState(false);
+  return (
+    <div>
+      {!testComplete ? (
+        <div className="testScreens">
+          <div className="timerDisplay"></div>
+          <div className="testParagraph"></div>
+          <input className="userInput"></input>
+        </div>
+      ) : (
+        <div className="resultScreen">
+          <div className="results"></div>
+          <button>Take test again</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-const generateRandomParagraph = (len) => {};
+const generateRandomParagraph = (len) => {
+  //Maybe generate a 200 word paragraph from words array
+};
+
 export default App;
